1be10f96c08735f5c2018ada87070f3c626f7c00
qi33 <qi33@purdue.edu>
2015-09-07 18:25:43 -0400

    Test push

--- Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/MainActivity.java
+++ Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    helloworld
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
