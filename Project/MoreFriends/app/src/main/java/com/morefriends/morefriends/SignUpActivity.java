10420de02023f6da00c42a05c7903bc9a302059d
tan126 <tan126@purdue.edu>
2015-09-25 17:22:24 -0400

    Add missing strings

--- Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/SignUpActivity.java
+++ Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/SignUpActivity.java
@@ -4,14 +4,22 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class SignUpActivity extends ActionBarActivity {
+
+    private EditText nickname;
+    private EditText email;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_account);
+        nickname = (EditText) findViewById(R.id.nick_name);
+        email = (EditText) findViewById(R.id.reset_email);
+        password = (EditText) findViewById(R.id.new_password);
     }
 
 }
