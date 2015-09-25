b604bb3472d77fe626a64cbf4aa170a24d92928e
cathy00 <zhan2032@purdue.edu>
2015-09-25 14:51:18 -0400

    Add viewpager and tabstrip

--- Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/MainActivity.java
+++ Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/MainActivity.java
@@ -1,20 +1,85 @@
 package com.morefriends.morefriends;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "Test", Toast.LENGTH_LONG).show();
+        ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter());
     }
+
+    public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+        final int PAGE_COUNT = 2;
+
+        public SampleFragmentPagerAdapter() {
+            super(getSupportFragmentManager());
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return PageFragment.create(position + 1);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return position == 0 ? "Search Friends" : "Chat List";
+        }
+    }
+
+    public static class PageFragment extends Fragment {
+        public static final String ARG_PAGE = "ARG_PAGE";
+
+        private int mPage;
+
+        public static PageFragment create(int page) {
+            Bundle args = new Bundle();
+            args.putInt(ARG_PAGE, page);
+            PageFragment fragment = new PageFragment();
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mPage = getArguments().getInt(ARG_PAGE);
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
+            //TextView textView = (TextView) view;
+            //textView.setText("Fragment #" + mPage);
+            return view;
+        }
+    }
+
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Test", Toast.LENGTH_LONG).show();
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
