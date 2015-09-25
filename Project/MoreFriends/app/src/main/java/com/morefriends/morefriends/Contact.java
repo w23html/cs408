ff1e3980151aca6fdd0d1ef552fe1331abc169aa
cathy00 <zhan2032@purdue.edu>
2015-09-25 18:15:33 -0400

    Add Contact class

---
+++ Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/Contact.java
@@ -0,0 +1,42 @@
+package com.morefriends.morefriends;
+
+/**
+ * Created by Cathy on 9/25/15.
+ */
+public class Contact {
+
+    String name = "";
+    String lastMessage = "";
+    String date = "";
+
+    public Contact(String name, String lastMessage, String date){
+        this.name = name;
+        this.lastMessage = lastMessage;
+        this.date = date;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastMessage(String date) {
+        this.lastMessage = lastMessage;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastMessage() {
+        return lastMessage;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+}
