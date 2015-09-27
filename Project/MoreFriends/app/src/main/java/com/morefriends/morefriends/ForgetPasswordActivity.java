c140d206a020b0d2f47ca0a9f75281a78b8903b1
cathy00 <zhan2032@purdue.edu>
2015-09-27 13:24:52 -0400

    Fix forget password activity

--- Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/ForgetPasswordActivity.java
+++ Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/ForgetPasswordActivity.java
@@ -4,19 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ForgetPasswordActivity extends Activity {
     private EditText email;
+    private TextView forgetPass;
+    private TextView cancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forget_password);
         email = (EditText) findViewById(R.id.reset_email);
-        email.setOnClickListener(new View.OnClickListener() {
+        forgetPass = (TextView) findViewById(R.id.reset_password);
+        cancel = (TextView) findViewById(R.id.cancel_reset);
+
+        forgetPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String em = email.getText().toString();
+                Toast.makeText(ForgetPasswordActivity.this, em, Toast.LENGTH_SHORT).show();
                 // TODO
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                onBackPressed();
             }
         });
 
