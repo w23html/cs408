7b299222aa4fd2a4025f4ebe5dedb3502b5244b3
w23html <qi33@purdue.edu>
2015-10-16 08:07:40 +0800

    Temporary test

--- Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/ScreenSlidePageFragment.java
+++ Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/ScreenSlidePageFragment.java
@@ -1,10 +1,12 @@
 package com.morefriends.morefriends;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class ScreenSlidePageFragment extends Fragment {
 
@@ -13,7 +15,14 @@ public class ScreenSlidePageFragment extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_screen_slide_page, container, false);
+        rootView.findViewById(R.id.button_go).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), FriendList.class);
+                startActivity(i);
 
+            }
+        });
         return rootView;
     }
 }
