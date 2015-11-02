82f3404f99741e5032937e57845be63cc76e367a
tan126 <tan126@purdue.edu>
2015-11-02 01:44:40 -0500

    Search random user when press start

--- Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/ScreenSlidePageFragment.java
+++ Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/ScreenSlidePageFragment.java
@@ -1,5 +1,6 @@
 package com.morefriends.morefriends;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -7,8 +8,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+import java.util.Random;
 
 public class ScreenSlidePageFragment extends Fragment {
+
+    void getRandomUser() {
+        final ProgressDialog dialog = ProgressDialog.show(getActivity(), null, "Searching...");
+        ParseQuery<ParseUser> pq = ParseUser.getQuery();
+        pq.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> objects, ParseException e) {
+                if (e == null) {
+                    int total = objects.size();
+                    Random ran = new Random();
+                    int idx = ran.nextInt(total);
+                    Intent i = new Intent(getActivity(), MainPage.class).putExtra("name", objects.get(idx).getUsername());
+                    startActivity(i);
+                    dialog.dismiss();
+                } else {
+                    dialog.dismiss();
+                    Toast.makeText(getActivity(), "No chats available.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,7 +49,7 @@ public class ScreenSlidePageFragment extends Fragment {
         rootView.findViewById(R.id.button_go).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                getRandomUser();
             }
         });
         return rootView;
