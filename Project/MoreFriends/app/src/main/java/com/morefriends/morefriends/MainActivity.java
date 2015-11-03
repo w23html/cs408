2fa8ee973d13c09f3e000391c8a00d940699b0ef
tan126 <tan126@purdue.edu>
2015-11-03 13:08:19 -0500

    main activity side menu

--- Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/MainActivity.java
+++ Project/MoreFriends/app/src/main/java/com/morefriends/morefriends/MainActivity.java
@@ -1,6 +1,7 @@
 package com.morefriends.morefriends;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
@@ -17,11 +18,15 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.parse.LogOutCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class MainActivity extends FragmentActivity {
@@ -38,15 +43,25 @@ public class MainActivity extends FragmentActivity {
 
          mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
          mLeftDrawer = (ListView) findViewById(R.id.left_drawer);
-
+         View v = getLayoutInflater().inflate(R.layout.sidemenuheader, null);
+         ((TextView)v.findViewById(R.id.name)).setText(ParseUser.getCurrentUser().getString("nickname"));
+         ((TextView)v.findViewById(R.id.email)).setText(ParseUser.getCurrentUser().getEmail());
+         if (ParseUser.getCurrentUser().getBytes("image") != null) {
+             CircleImageView iv = (CircleImageView) v.findViewById(R.id.image);
+             iv.setImageBitmap(BitmapFactory.decodeByteArray(ParseUser.getCurrentUser().getBytes("image"), 0, ParseUser.getCurrentUser().getBytes("image").length));
+         } else {
+             CircleImageView iv = (CircleImageView) v.findViewById(R.id.image);
+             iv.setImageResource(R.drawable.anon_54);
+         }
+         mLeftDrawer.addHeaderView(v);
          mLeftDrawer.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuItems));
          mLeftDrawer.setOnItemClickListener(new AdapterView.OnItemClickListener() {
              @Override
              public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 if (position == 0) {
+                 if (position == 1) {
                      Intent i = new Intent(MainActivity.this, PersonalInfo.class);
                      startActivity(i);
-                 } else {
+                 } else if (position == 2) {
                      ParseUser.getCurrentUser().logOutInBackground(new LogOutCallback() {
                          @Override
                          public void done(ParseException e) {
